c12523947bd31008ece2d517536a6795d3e4bffb
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-24 12:49:41 +0200

    Creating Navbar.js file

---
+++ src/components/HomePage/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components"
+
+function Navbar() {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export default Navbar;
