addfb615cdf18ec3dd1fa4a66c3c5b9c02e25b98
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-24 12:44:28 +0200

    Creating HomePageCompound.js file

---
+++ src/compounds/HomePageCompound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components"
+
+function HomePageCompound() {
+    return (
+        <Wrapper>
+          
+        </Wrapper>
+    );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export default HomePageCompound;
