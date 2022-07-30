138fa051f23966e6ec241f23c8a4b36834a8e345
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-30 13:23:24 +0200

    Defining Communicate.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/HomePage/Navbar'
+import Communicate from '../components/HomePage/Communicate'
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Communicate />
     </Wrapper>
   );
 }
