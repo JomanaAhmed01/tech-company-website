4e23f4f45254f572315abb31e7b595c6965a67a5
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-31 17:55:08 +0200

    Defining RightSoftware.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/HomePage/Navbar'
 import Communicate from '../components/HomePage/Communicate'
+import RightSoftware from '../components/HomePage/RightSoftware'
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Communicate />
+      <RightSoftware />
     </Wrapper>
   );
 }
