fcae17e375844610a97418279488456319d6b811
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-01 14:18:49 +0200

    Adjusting the code

--- src/components/HomePage/BuiltForCreatives.js
+++ src/components/HomePage/BuiltForCreatives.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import styled from "styled-components"
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
 </style>
 
 function BuiltForCreatives() {
-    return (
-        <Wrapper>
-            <ImageWrapper>
-                <Image src='./img/contact.svg' />
-            </ImageWrapper>
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Image src='./img/contact.svg' />
+      </ImageWrapper>
 
-            <TextWrapper>
-                <Header>Built for Creatives, by Creatives</Header>
-                <Text>I'm a paragraph.
-                    Click here to add your own text and edit me.
-                    It’s easy.
-                    Just click “Edit Text” or double click me to add your own content and make changes to the font.
-                    I’m a great place for you to tell a story and let your users know a little more about you.
-                </Text>
+      <TextWrapper>
+        <Header>Built for Creatives, by Creatives</Header>
+        <Text>I'm a paragraph.
+          Click here to add your own text and edit me.
+          It’s easy.
+          Just click “Edit Text” or double click me to add your own content and make changes to the font.
+          I’m a great place for you to tell a story and let your users know a little more about you.
+        </Text>
 
-                <IconsWrapper>
-                    <IconTextWrapper>
-                        <Icon src='./img/all-in-one.svg' />
-                        <IconText>All-In-One Toolkit</IconText>
-                    </IconTextWrapper>
+        <IconsWrapper>
+          <IconTextWrapper>
+            <Icon src='./img/all-in-one.svg' />
+            <IconText>All-In-One Toolkit</IconText>
+          </IconTextWrapper>
 
-                    <IconTextWrapper>
-                        <Icon src='./img/file-sharing.svg' />
-                        <IconText>Integrated File Sharing</IconText>
-                    </IconTextWrapper>
+          <IconTextWrapper>
+            <Icon src='./img/file-sharing.svg' />
+            <IconText>Integrated File Sharing</IconText>
+          </IconTextWrapper>
 
-                    <IconTextWrapper>
-                        <Icon src='./img/design-freedom.svg' />
-                        <IconText>Total Design Freedom</IconText>
-                    </IconTextWrapper>
-                </IconsWrapper>
-            </TextWrapper>
-        </Wrapper>
-    );
+          <IconTextWrapper>
+            <Icon src='./img/design-freedom.svg' />
+            <IconText>Total Design Freedom</IconText>
+          </IconTextWrapper>
+        </IconsWrapper>
+      </TextWrapper>
+    </Wrapper>
+  );
 }
 
 export const Wrapper = styled.div`
