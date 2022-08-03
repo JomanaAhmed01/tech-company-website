07f0d8e712cfa47899acc2b200a28d1bcca38710
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 08:07:43 +0200

    Defining Products.js file in the ProductsPageCompound.js file

--- src/compounds/ProductPageCompound.js
+++ src/compounds/ProductPageCompound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from "styled-components"
-import Navbar from '../components/ProductPage/Navbar';
+import Navbar from '../components/ProductPage/Navbar'
+import Products from '../components/ProductPage/Products'
 
 function ProductPageCompound() {
     return (
         <Wrapper>
             <Navbar />
+            <Products />
         </Wrapper>
     );
 }
