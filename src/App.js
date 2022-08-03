ec1a03901887852b6ab18d3a5e5606abeb057aed
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 10:37:34 +0200

    Defining ContactPageCompound.js file in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 // import HomePageCompound from './compounds/HomePageCompound'
 // import ProductPageCompound from './compounds/ProductPageCompound'
-import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
+// import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
+import ContactPageCompound from './compounds/ContactPageCompound'
 
 function App() {
   return (
     <div className="App">
       {/* <HomePageCompound /> */}
       {/* <ProductPageCompound /> */}
-      <PricingPlansPageCompound />
+      {/* <PricingPlansPageCompound /> */}
+      <ContactPageCompound />
     </div>
   );
 }
