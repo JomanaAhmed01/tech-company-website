d35f6bdb4362aa48268d8d8ae9ee4b3185c2104f
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 08:36:26 +0200

    Defining Navbar.js file in the PricingPlansPageCompound.js file

--- src/compounds/PricingPlansPageCompound.js
+++ src/compounds/PricingPlansPageCompound.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import styled from "styled-components"
+import Navbar from '../components/PricingPlansPage/Navbar'
 
 function PricingPlansPageCompound() {
     return (
         <Wrapper>
-
+            <HeaderSectionWrapper>
+                <Navbar />
+            </HeaderSectionWrapper>
         </Wrapper>
     );
 }
 
 export const Wrapper = styled.div`
   
+`
+
+export const HeaderSectionWrapper = styled.div`
+  height: auto;
+  background-color: #FFD7EF;
 `
 
 export default PricingPlansPageCompound;
