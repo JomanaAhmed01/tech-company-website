c3a02e55050ba2ede1be2b7006ac3836711d5c0d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 08:10:53 +0200

    Defining Footer.js file in the ProductsPageCompound.js file

--- src/compounds/ProductPageCompound.js
+++ src/compounds/ProductPageCompound.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/ProductPage/Navbar'
 import Products from '../components/ProductPage/Products'
+import Footer from '../components/ProductPage/Footer'
 
 function ProductPageCompound() {
     return (
         <Wrapper>
             <Navbar />
             <Products />
+            <Footer />
         </Wrapper>
     );
 }
