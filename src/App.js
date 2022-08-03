7ad33b3582bbe7e439e7d53822a5bdec27e8113d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 11:15:44 +0200

    Hiding DemoModePageCompound.js file & Showing the HomePageCompound.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-// import HomePageCompound from './compounds/HomePageCompound'
+import HomePageCompound from './compounds/HomePageCompound'
 // import ProductPageCompound from './compounds/ProductPageCompound'
 // import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
 // import ContactPageCompound from './compounds/ContactPageCompound'
-import DemoModePageCompound from './compounds/DemoModePageCompound'
+// import DemoModePageCompound from './compounds/DemoModePageCompound'
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePageCompound /> */}
+      <HomePageCompound />
       {/* <ProductPageCompound /> */}
       {/* <PricingPlansPageCompound /> */}
       {/* <ContactPageCompound /> */}
-      <DemoModePageCompound />
+      {/* <DemoModePageCompound /> */}
     </div>
   );
 }
