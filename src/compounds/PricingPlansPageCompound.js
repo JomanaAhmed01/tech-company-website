90251e52c74e73a14b8c6a3af37cfb70114aaf3c
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 08:56:55 +0200

    Defining Header.js file in the PricingPlansPageCompound.js file

--- src/compounds/PricingPlansPageCompound.js
+++ src/compounds/PricingPlansPageCompound.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/PricingPlansPage/Navbar'
+import Header from '../components/PricingPlansPage/Header'
 
 function PricingPlansPageCompound() {
     return (
         <Wrapper>
             <HeaderSectionWrapper>
                 <Navbar />
+                <Header />
             </HeaderSectionWrapper>
         </Wrapper>
     );
