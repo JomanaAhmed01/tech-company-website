152756cb37b69109ca6285c2010965329431d7b2
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-03 08:16:19 +0200

    Defining PricingPlansPageCompound.js file in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import HomePageCompound from './compounds/HomePageCompound'
-import ProductPageCompound from './compounds/ProductPageCompound'
+// import ProductPageCompound from './compounds/ProductPageCompound'
+import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
 
 function App() {
   return (
     <div className="App">
       {/* <HomePageCompound /> */}
-      <ProductPageCompound />
+      {/* <ProductPageCompound /> */}
+      <PricingPlansPageCompound />
     </div>
   );
 }
