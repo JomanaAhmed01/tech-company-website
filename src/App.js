f5615465bb4a437d7f07944b3e01c6271dd34b2a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-04 11:07:15 +0200

    Adding react-router-dom to the App.js file & Defining it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePageCompound from './compounds/HomePageCompound'
-// import ProductPageCompound from './compounds/ProductPageCompound'
-// import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
-// import ContactPageCompound from './compounds/ContactPageCompound'
-// import DemoModePageCompound from './compounds/DemoModePageCompound'
+import ProductPageCompound from './compounds/ProductPageCompound'
+import PricingPlansPageCompound from './compounds/PricingPlansPageCompound'
+import ContactPageCompound from './compounds/ContactPageCompound'
+import DemoModePageCompound from './compounds/DemoModePageCompound'
 
 function App() {
   return (
     <div className="App">
-      <HomePageCompound />
-      {/* <ProductPageCompound /> */}
-      {/* <PricingPlansPageCompound /> */}
-      {/* <ContactPageCompound /> */}
-      {/* <DemoModePageCompound /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePageCompound />
+          </Route>
+
+          <Route path="/ProductPageCompound">
+            <ProductPageCompound />
+          </Route>
+
+          <Route path="/PricingPlansPageCompound">
+            <PricingPlansPageCompound />
+          </Route>
+
+          <Route path="/ContactPageCompound">
+            <ContactPageCompound />
+          </Route>
+
+          <Route path="/DemoModePageCompound">
+            <DemoModePageCompound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
