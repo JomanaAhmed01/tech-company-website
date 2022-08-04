6ffbe63db65370ce70846200cf33600174699889
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-04 13:44:11 +0200

    Creating CheckoutPageCompound.js file

---
+++ src/compounds/CheckoutPageCompound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components"
+
+function CheckoutPageCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export default CheckoutPageCompound;
