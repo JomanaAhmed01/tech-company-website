c6bd75ddfd89a48d55f2990dbb67cbe35179ab3e
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-04 13:59:59 +0200

    Defining Navbar.js file in the App.js file & Adding background-color to the Navbar section

--- src/compounds/CheckoutPageCompound.js
+++ src/compounds/CheckoutPageCompound.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import styled from "styled-components"
+import Navbar from '../components/CheckoutPage/Navbar'
 
 function CheckoutPageCompound() {
   return (
     <Wrapper>
-      
+      <HeaderSectionWrapper>
+        <Navbar />
+      </HeaderSectionWrapper>
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.div`
   
+`
+
+export const HeaderSectionWrapper = styled.div`
+  height: 300px;
+  background-color: #FFD7EF;
 `
 
 export default CheckoutPageCompound;
