300437cab121da8c74bc00fb945d63e7cb70155a
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-09 13:44:13 +0200

    Defining MobileMenu.js file in the HomePageCompound.js file & Making the Home page disappear in 1024px width & only show the Mobile menu section

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/HomePage/Navbar'
+import MobileMenu from '../components/HomePage/MobileMenu'
 import Communicate from '../components/HomePage/Communicate'
 import RightSoftware from '../components/HomePage/RightSoftware'
 import WhatWeOffer from '../components/HomePage/WhatWeOffer'
@@ -14,22 +15,31 @@ import Footer from '../components/HomePage/Footer'
 function HomePageCompound() {
   return (
     <Wrapper>
-      <Navbar />
-      <Communicate />
-      <RightSoftware />
-      <WhatWeOffer />
-      <BuiltForCreatives />
-      <IndustryLeaders />
-      <ClientsReview />
-      <PricingOptions />
-      <MaximizeYourProductivity />
-      <Footer />
+      <HeaderWrapper>
+        <Navbar />
+        <Communicate />
+        <RightSoftware />
+        <WhatWeOffer />
+        <BuiltForCreatives />
+        <IndustryLeaders />
+        <ClientsReview />
+        <PricingOptions />
+        <MaximizeYourProductivity />
+        <Footer />
+      </HeaderWrapper>
+      <MobileMenu />
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.div`
   
+`
+
+export const HeaderWrapper = styled.div`
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
 `
 
 export default HomePageCompound;
