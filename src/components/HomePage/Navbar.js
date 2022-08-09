b77c6f33325a3a3552ef5c88033fbf8f8b34f11d
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-09 21:37:26 +0200

    Moving MobileMenu.js file's code to the Navbar.js file

--- src/components/HomePage/Navbar.js
+++ src/components/HomePage/Navbar.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 import { useHistory } from 'react-router-dom'
 import { PersonCircle } from '@styled-icons/ionicons-solid/PersonCircle'
 import { MenuOutline } from '@styled-icons/evaicons-outline/MenuOutline'
-import MobileMenu from '../HomePage/MobileMenu'
+import { Delete } from '@styled-icons/typicons/Delete'
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
 </style>
@@ -20,7 +20,23 @@ function Navbar() {
 
       <>
         {showMenuSection ? (
-          <MobileMenu />
+          <MenuWrapper>
+            <LogInOutWrapper>
+              <LogInProfileWrapper>
+                <ProfileIcon onClick={() => history.push('/DemoModePageCompound')} />
+                <LogInText onClick={() => history.push('/DemoModePageCompound')}>Log In</LogInText>
+              </LogInProfileWrapper>
+
+              <ExitIcon onClick={() => history.push('/')} />
+            </LogInOutWrapper>
+
+            <MenuItemsWrapper>
+              <MenuFirstItem onClick={() => history.push('/')}>Home</MenuFirstItem>
+              <MenuItem onClick={() => history.push('/ProductPageCompound')}>Product</MenuItem>
+              <MenuItem onClick={() => history.push('/PricingPlansPageCompound')}>Pricing Plans</MenuItem>
+              <MenuItem onClick={() => history.push('/ContactPageCompound')}>Contact</MenuItem>
+            </MenuItemsWrapper>
+          </MenuWrapper>
         ) : (
           <>
             <LogoWrapper>
@@ -47,7 +63,7 @@ function Navbar() {
               </ButtonWrapper>
             </ItemsWrapper>
 
-            <MenuIcon />
+            <MenuIcon onClick={() => setShowMenuSection(true)} />
           </>
         )}
       </>
@@ -175,6 +191,88 @@ export const MenuIcon = styled(MenuOutline)`
     width: 40px;
     height: 40px;
   }
+`
+
+export const MenuWrapper = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 90%;
+    overflow: hidden;
+    margin-top: 50px;
+  }
+`
+
+export const MenuItemsWrapper = styled.div`
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+    align-items: flex-start;
+    height: auto;
+    background-color: #FFFFFF;
+    margin-left: 50px;
+  }
+`
+
+export const MenuFirstItem = styled.p`
+  @media screen and (max-width: 1024px) {
+    color: #6E6E6E;
+    font-size: 22px;
+    font-family: 'Space Grotesk', sans-serif;
+    font-weight: bold;
+    padding-bottom: 10px;
+  }
+`
+
+export const MenuItem = styled.p`
+  color: #000000;
+  font-size: 16px;
+  transition: 0.3s ease-in-out;
+  padding-bottom: 10px;
+  cursor: default;
+
+  :hover {
+    color: #6E6E6E;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 22px;
+    font-family: 'Space Grotesk', sans-serif;
+    font-weight: bold;
+  }
+`
+
+export const LogInOutWrapper = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1024px) {
+    display: initial;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 50px;
+  }
+`
+
+export const LogInProfileWrapper = styled.div`
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const LogInText = styled.p`
+  font-size: 16px;
+  font-family: 'Space Grotesk', sans-serif;
+`
+
+export const ExitIcon = styled(Delete)`
+  width: 35px;
+  height: 38px;
+  cursor: pointer;
+  margin-right: 50px;
 `
 
 export default Navbar;
