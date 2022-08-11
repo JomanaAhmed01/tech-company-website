83e53a2cc08ec2afc3d1d19ecf2ea45f08929e69
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-08-11 11:26:53 +0200

    Adding Mobile menu to the Pricing plans page's navbar section

--- src/components/PricingPlansPage/Navbar.js
+++ src/components/PricingPlansPage/Navbar.js
@@ -1,43 +1,72 @@
 import React from 'react';
 import styled from "styled-components"
+import { useState } from "react"
 import { useHistory } from 'react-router-dom'
 import { PersonCircle } from '@styled-icons/ionicons-solid/PersonCircle'
 import { MenuOutline } from '@styled-icons/evaicons-outline/MenuOutline'
+import { Delete } from '@styled-icons/typicons/Delete'
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Dancing+Script:wght@500;700&family=Montserrat:wght@200;300;400&family=Poppins&family=Questrial&family=Quicksand:wght@300&family=Raleway:wght@100&family=Roboto:wght@300&family=Space+Grotesk&family=Suez+One&display=swap');
 </style>
 
 function Navbar() {
 
+  const [showMenuSection, setShowMenuSection] = useState(false)
+
   const history = useHistory()
 
   return (
     <Wrapper>
-      <LogoWrapper>
-        <Logo src='./img/logo.svg' />
-        <LogoTextWrapper>
-          <WeDu onClick={() => history.push('/')}>WeDu</WeDu>
-          <Desc>Communicate. Collaborate. Create.</Desc>
-        </LogoTextWrapper>
-      </LogoWrapper>
 
-      <ItemsWrapper>
-        <Item onClick={() => history.push('/')}>Home</Item>
-        <Item onClick={() => history.push('/ProductPageCompound')}>Product</Item>
-        <ThirdItem onClick={() => history.push('/PricingPlansPageCompound')}>Pricing Plans</ThirdItem>
-        <Item onClick={() => history.push('/ContactPageCompound')}>Contact</Item>
+      <>
+        {showMenuSection ? (
+          <MenuWrapper>
+            <LogInOutWrapper>
+              <LogInProfileWrapper>
+                <ProfileIcon onClick={() => history.push('/DemoModePageCompound')} />
+                <LogInText onClick={() => history.push('/DemoModePageCompound')}>Log In</LogInText>
+              </LogInProfileWrapper>
 
-        <LogInWrapper>
-          <ProfileIcon onClick={() => history.push('/DemoModePageCompound')} />
-          <Item onClick={() => history.push('/DemoModePageCompound')}>Log In</Item>
-        </LogInWrapper>
+              <ExitIcon onClick={() => setShowMenuSection(false)} />
+            </LogInOutWrapper>
 
-        <ButtonWrapper>
-          <Button onClick={() => history.push('/PricingPlansPageCompound')}>Get Started</Button>
-        </ButtonWrapper>
-      </ItemsWrapper>
+            <MenuItemsWrapper>
+              <MenuItem onClick={() => history.push('/')}>Home</MenuItem>
+              <MenuItem onClick={() => history.push('/ProductPageCompound')}>Product</MenuItem>
+              <MenuThirdItem onClick={() => history.push('/PricingPlansPageCompound')}>Pricing Plans</MenuThirdItem>
+              <MenuItem onClick={() => history.push('/ContactPageCompound')}>Contact</MenuItem>
+            </MenuItemsWrapper>
+          </MenuWrapper>
+        ) : (
+          <>
+            <LogoWrapper>
+              <Logo src='./img/logo.svg' />
+              <LogoTextWrapper>
+                <WeDu onClick={() => history.push('/')}>WeDu</WeDu>
+                <Desc>Communicate. Collaborate. Create.</Desc>
+              </LogoTextWrapper>
+            </LogoWrapper>
 
-      <MenuIcon />
+            <ItemsWrapper>
+              <Item onClick={() => history.push('/')}>Home</Item>
+              <SecondItem onClick={() => history.push('/ProductPageCompound')}>Product</SecondItem>
+              <Item onClick={() => history.push('/PricingPlansPageCompound')}>Pricing Plans</Item>
+              <Item onClick={() => history.push('/ContactPageCompound')}>Contact</Item>
+
+              <LogInWrapper>
+                <ProfileIcon onClick={() => history.push('/DemoModePageCompound')} />
+                <Item onClick={() => history.push('/DemoModePageCompound')}>Log In</Item>
+              </LogInWrapper>
+
+              <ButtonWrapper>
+                <Button onClick={() => history.push('/PricingPlansPageCompound')}>Get Started</Button>
+              </ButtonWrapper>
+            </ItemsWrapper>
+
+            <MenuIcon onClick={() => setShowMenuSection(true)} />
+          </>
+        )}
+      </>
     </Wrapper>
   );
 }
@@ -95,7 +124,7 @@ export const ItemsWrapper = styled.div`
   }
 `
 
-export const ThirdItem = styled.p`
+export const SecondItem = styled.p`
   color: #6E6E6E;
   font-size: 16px;
   cursor: default;
@@ -162,6 +191,87 @@ export const MenuIcon = styled(MenuOutline)`
     width: 40px;
     height: 40px;
   }
+`
+
+export const MenuWrapper = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    margin-top: 50px;
+  }
+`
+
+export const MenuItemsWrapper = styled.div`
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+    align-items: flex-start;
+    height: 400px;
+    margin-left: 50px;
+  }
+`
+
+export const MenuThirdItem = styled.p`
+  @media screen and (max-width: 1024px) {
+    color: #6E6E6E;
+    font-size: 22px;
+    font-family: 'Space Grotesk', sans-serif;
+    font-weight: bold;
+    padding-bottom: 10px;
+  }
+`
+
+export const MenuItem = styled.p`
+  color: #000000;
+  font-size: 16px;
+  transition: 0.3s ease-in-out;
+  padding-bottom: 10px;
+  cursor: default;
+
+  :hover {
+    color: #6E6E6E;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 22px;
+    font-family: 'Space Grotesk', sans-serif;
+    font-weight: bold;
+  }
+`
+
+export const LogInOutWrapper = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1024px) {
+    display: initial;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 50px;
+  }
+`
+
+export const LogInProfileWrapper = styled.div`
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const LogInText = styled.p`
+  font-size: 16px;
+  font-family: 'Space Grotesk', sans-serif;
+`
+
+export const ExitIcon = styled(Delete)`
+  width: 35px;
+  height: 38px;
+  cursor: pointer;
+  margin-right: 50px;
 `
 
 export default Navbar;
